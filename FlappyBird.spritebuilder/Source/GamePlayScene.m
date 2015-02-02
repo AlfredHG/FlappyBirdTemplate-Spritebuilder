10c24328f6244dd357acc70d9ea805d460d97b1e
Fabio Dono <fabio.d@ulixe.to>
2015-02-02 13:00:39 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,13 +7,28 @@
 - (void)initialize
 {
     // your code here
+    character = (Character *)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
+    
+    [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta; //delta e` circa 1/60th di secondo
+    
+    if (timeSinceObstacle > 2.0f) {
+        [self addObstacle];
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 // put new methods here
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+    // this will get called every time the player touches the screen
+    [character flap];
+}
 
 @end
